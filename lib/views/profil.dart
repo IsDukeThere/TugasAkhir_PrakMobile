8470cef4ea65a7a4cb468ce07a6461a97d82b008
RudiAhmad <rudi.ahmad31@gmail.com>
2025-12-03 11:23:04 +0700

    tambah fitur ganti password

--- lib/views/profil.dart
+++ lib/views/profil.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:project_akhir/views/login.dart';
+import 'package:hive/hive.dart';
+import 'package:project_akhir/utils/encryption.dart';
 
 class Profil extends StatefulWidget {
   final String user;
@@ -18,11 +20,13 @@ class _ProfilState extends State<Profil> {
   @override
   void initState() {
     super.initState();
-    _nama = widget.user; 
+    _nama = widget.user;
   }
 
-  void _showEditNameDialog() {
-    TextEditingController _controller = TextEditingController(text: _nama);
+  // Dialog untuk ganti password
+  void _showChangePasswordDialog() {
+    final oldPasswordController = TextEditingController();
+    final newPasswordController = TextEditingController();
 
     showDialog(
       context: context,
@@ -31,19 +35,39 @@ class _ProfilState extends State<Profil> {
           backgroundColor: Colors.black,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(15.0),
-            side: const BorderSide(color: Color.fromARGB(255, 37, 216, 101), width: 1),
+            side: const BorderSide(
+                color: Color.fromARGB(255, 37, 216, 101), width: 1),
           ),
-          title: const Text("Ubah Username", style: TextStyle(color: Colors.white)),
-          content: TextField(
-            controller: _controller,
-            style: const TextStyle(color: Colors.white),
-            decoration: const InputDecoration(
-              hintText: "Masukkan Username baru",
-              hintStyle: TextStyle(color: Colors.grey),
-              enabledBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: Colors.white),
+          title: const Text("Ganti Password",
+              style: TextStyle(color: Colors.white)),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: oldPasswordController,
+                obscureText: true,
+                style: const TextStyle(color: Colors.white),
+                decoration: const InputDecoration(
+                  hintText: "Password lama",
+                  hintStyle: TextStyle(color: Colors.grey),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                ),
               ),
-            ),
+              TextField(
+                controller: newPasswordController,
+                obscureText: true,
+                style: const TextStyle(color: Colors.white),
+                decoration: const InputDecoration(
+                  hintText: "Password baru",
+                  hintStyle: TextStyle(color: Colors.grey),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
           ),
           actions: [
             TextButton(
@@ -51,14 +75,16 @@ class _ProfilState extends State<Profil> {
               child: const Text("Batal", style: TextStyle(color: Colors.grey)),
             ),
             ElevatedButton(
-              style: ElevatedButton.styleFrom(backgroundColor: Color.fromARGB(255, 37, 216, 101),),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 37, 216, 101),
+              ),
               onPressed: () {
-                setState(() {
-                  _nama = _controller.text;
-                });
+                _changePassword(
+                    oldPasswordController.text, newPasswordController.text);
                 Navigator.pop(context);
               },
-              child: const Text("Simpan", style: TextStyle(color: Colors.white)),
+              child: const Text("Confirm",
+                  style: TextStyle(color: Colors.white)),
             ),
           ],
         );
@@ -66,96 +92,126 @@ class _ProfilState extends State<Profil> {
     );
   }
 
+  // Fungsi update password di Hive
+  void _changePassword(String oldPassword, String newPassword) {
+    var box = Hive.box('users');
+    if (box.containsKey(_nama)) {
+      final savedHash = box.get(_nama);
+      final oldHash = hashPassword(oldPassword);
+
+      if (savedHash == oldHash) {
+        final newHash = hashPassword(newPassword);
+        box.put(_nama, newHash);
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            backgroundColor: Colors.green,
+            content: Text("Password berhasil diganti!",
+                style: TextStyle(color: Colors.white)),
+          ),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            backgroundColor: Colors.red,
+            content: Text("Password lama salah!",
+                style: TextStyle(color: Colors.white)),
+          ),
+        );
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Profil",
-          style: TextStyle(
-            color: Colors.white),
-        ),
-        actions: [
-          IconButton(onPressed: (){
-            Navigator.pushReplacement(
-                context, 
-                MaterialPageRoute(
-                  builder: (context) {
-                    return LoginPage();
-                  }
+        appBar: AppBar(
+          title: const Text(
+            "Profil",
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return const LoginPage();
+                    },
                   ),
                 );
-          }, 
-          icon: FaIcon(FontAwesomeIcons.rightFromBracket, color: Colors.white),
-          )
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.only(top: 15, bottom: 15),
-                height: 150,
-                width: 150,
-                decoration: BoxDecoration(
-                  image: DecorationImage(image: AssetImage(
-                    "assets/img/duki.jpg"
-                  ), fit: BoxFit.cover,
-                  alignment: Alignment.topCenter),
-                  borderRadius: BorderRadius.circular(100),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.3),
-                      blurRadius: 8,
-                      offset: const Offset(0, 4),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 20),
-              _infoRow("Nama", _nama),
-              _infoRow("NIM", _nim),
-              _infoRow("Kelas", _kelas),
-              SizedBox(height: 40),
-              SizedBox(
-                width: double.infinity,
-                height: 50,
-                child: ElevatedButton(
-                  onPressed: _showEditNameDialog,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color.fromARGB(255, 37, 216, 101),
-                    foregroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                  ),
-                  child: const Text(
-                    "Edit Profil",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-            ],
-          ),
+              },
+              icon: const FaIcon(FontAwesomeIcons.rightFromBracket,
+                  color: Colors.white),
+            )
+          ],
         ),
-      )
-    );
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 15, bottom: 15),
+                  height: 150,
+                  width: 150,
+                  decoration: BoxDecoration(
+                    image: const DecorationImage(
+                        image: AssetImage("assets/img/duki.jpg"),
+                        fit: BoxFit.cover,
+                        alignment: Alignment.topCenter),
+                    borderRadius: BorderRadius.circular(100),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.3),
+                        blurRadius: 8,
+                        offset: const Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                _infoRow("Nama", _nama),
+                _infoRow("NIM", _nim),
+                _infoRow("Kelas", _kelas),
+                const SizedBox(height: 40),
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: _showChangePasswordDialog,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    child: const Text(
+                      "Ganti Password",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 
-Widget _infoRow(String label, String value, {bool isEditable = false, VoidCallback? onEdit}) {
+  Widget _infoRow(String label, String value,
+      {bool isEditable = false, VoidCallback? onEdit}) {
     return Container(
       padding: const EdgeInsets.all(5),
       width: double.infinity,
-      decoration: BoxDecoration(
-      ),
       child: Row(
         children: [
           SizedBox(
-            width: 80, // Lebar tetap untuk label agar rapi
+            width: 80,
             child: Text(
-              "$label",
+              label,
               style: const TextStyle(
                   fontWeight: FontWeight.bold, color: Colors.white),
             ),
@@ -169,13 +225,6 @@ Widget _infoRow(String label, String value, {bool isEditable = false, VoidCallba
               overflow: TextOverflow.ellipsis,
             ),
           ),
-          // Jika isEditable true, tampilkan icon pensil
-          if (isEditable)
-            IconButton(
-              icon: const Icon(Icons.edit, color: Colors.amber, size: 20),
-              onPressed: onEdit,
-              tooltip: "Ubah Nama",
-            )
         ],
       ),
     );
